1c580aabf7770b3a61c74db67417f749b316a3a4
KJM <kunalmahajan0202@gmail.com>
2026-03-13 15:00:11 +0530

    update url of backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/api',
+  baseURL: 'https://f3-phb-food-redis.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
