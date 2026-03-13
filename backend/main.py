18cf5e751d69cf3085ed5914d3283d6febd0162c
KJM <kunalmahajan0202@gmail.com>
2026-03-13 15:25:17 +0530

    update admin login

--- backend/main.py
+++ backend/main.py
@@ -2,19 +2,24 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routers import router
-from database import engine
+from database import engine, SessionLocal
 import models
+from auth import hash_password
 
-# create tables
+
+# ---------------- CREATE TABLES ----------------
 models.Base.metadata.create_all(bind=engine)
 
+
+# ---------------- CREATE APP ----------------
 app = FastAPI(
     title="Hunger Relief Network",
     description="API for food donation, inspection and distribution",
     version="1.0.0"
 )
 
-# CORS (required for React frontend)
+
+# ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -23,10 +28,40 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# include routers
+
+# ---------------- AUTO CREATE ADMIN ----------------
+def create_admin():
+    db = SessionLocal()
+
+    admin = db.query(models.User).filter(models.User.role == "admin").first()
+
+    if not admin:
+        new_admin = models.User(
+            fullname="System Admin",
+            email="admin123@gmail.com",
+            password=hash_password("admin123"),
+            role="admin"
+        )
+
+        db.add(new_admin)
+        db.commit()
+        print("Admin created successfully")
+
+    else:
+        print("Admin already exists")
+
+    db.close()
+
+
+# Run when server starts
+create_admin()
+
+
+# ---------------- ROUTERS ----------------
 app.include_router(router, prefix="/api")
 
 
+# ---------------- ROOT ----------------
 @app.get("/")
 def root():
     return {"message": "Hunger Relief Network API running"}
